d37a94202d2e26f18b0e97704f0132a8067bf2a1
IsmaelSailema20 <ismaelsailema110@gmail.com>
2024-10-25 16:57:23 -0500

    Creación de la pagina para las Sugerencias y votos

---
+++ src/PaginaSugerenciasVotos.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import FormularioSugerencias from "./FormularioSugerencias.jsx";
+function PaginaSugerenciasVotos() {
+  return (
+    <div className="bg-slate-100 w-1/2 p-6 m-8 rounded-xl shadow-xl ">
+      <FormularioSugerencias></FormularioSugerencias>
+    </div>
+  );
+}
+
+export default PaginaSugerenciasVotos;
