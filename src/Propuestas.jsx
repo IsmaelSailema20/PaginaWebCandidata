6e82e9bc624ebe5fc7109e5c560bca6d53848452
Reclax <marcoserrano2206@gmail.com>
2024-10-28 21:11:00 -0500

    Se crea la estructura inicial del componente Propuestas configurando la base del componente React con su estructura básica del layout con flexbox incluida una división en sección izquierda y derecha

---
+++ src/Propuestas.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Propuestas = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="flex flex-col md:flex-row">
+        <div className="w-full md:w-5/12">
+          {/* Left section */}
+        </div>
+        <div className="w-full md:w-7/12">
+          {/* Right section */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Propuestas;
