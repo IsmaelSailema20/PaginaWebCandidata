4be80baf5c6b560e8563ee318db15d5ecd963803
Ismael Sailema <ismaelsailema110@gmail.com>
2024-10-30 08:22:19 +0000

    Revert "Merge branch 'feature/pagina-home' into 'main'"
    This reverts merge request !7

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './home.jsx'
-import NavBar from './NavBar.jsx'
+import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <NavBar />
-    <Home />
+    <App />
   </StrictMode>,
 )
