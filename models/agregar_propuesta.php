19be18ebbec70eff63a96306966d45c5ef356c3e
Reclax <marcoserrano2206@gmail.com>
2024-12-15 03:02:28 -0500

    Se corrige error en la conexion del model agregar_propuesta.php

--- models/agregar_propuesta.php
+++ models/agregar_propuesta.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Credentials: true');
