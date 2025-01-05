d3a6bbb86112c629623bfaf969a509a8435a824c
Your Name <alejosanchez456@gmail.com>
2025-01-05 18:51:46 -0500

    arreglando la seccion de noticias y eventos

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -47,31 +47,52 @@ function Home() {
       .catch((error) => console.error("Error al obtener secciones:", error));
   }, []);
 
-  // Obtener noticias
   useEffect(() => {
     fetch(`${API_BASE_URL}/get_news.php`)
       .then((response) => response.json())
-      .then((data) => setNews(data))
+      .then((data) => {
+        const updatedNews = data.map(item => {
+          const words = item.descrip_evento_noticia.split(" ");
+          const truncatedDescription = words.slice(0, 10).join(" "); // Cortar las primeras 10 palabras
+          return {
+            ...item,
+            descrip_evento_noticia: truncatedDescription.endsWith("...")
+              ? truncatedDescription // Si ya tiene puntos al final, no hacer nada
+              : `${truncatedDescription}...` // Si no, agregar exactamente tres puntos
+          };
+        });
+        setNews(updatedNews); // Actualizar el estado con las noticias procesadas.
+        console.log(data);
+      })
       .catch((error) => console.error("Error al obtener noticias:", error));
+  }, []);
+
+  useEffect(() => {
+    fetch(`${API_BASE_URL}/get_events.php`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const updatedEvents = data.map(item => {
+          const words = item.descrip_evento_noticia.split(" ");
+          const truncatedDescription = words.slice(0, 10).join(" "); // Cortar las primeras 10 palabras
+          return {
+            ...item,
+            descrip_evento_noticia: truncatedDescription.endsWith("...")
+              ? truncatedDescription // Si ya tiene puntos al final, no hacer nada
+              : `${truncatedDescription}...` // Si no, agregar exactamente tres puntos
+          };
+        });
+        setEvents(updatedEvents); // Actualizar el estado con los eventos procesados.
+        console.log("Eventos:");
+        console.log(data);
+      })
+      .catch((error) => console.error("Error al obtener eventos:", error));
   }, []);
   useEffect(() => {
     fetch(`${API_BASE_URL}/get_informacion_partido.php`)
       .then((response) => response.json())
       .then((data) => setInformacion(data))
       .catch((error) => console.error("Error al obtener noticias:", error));
-  }, []);
-  // Obtener eventos (solo los 3 primeros)
-  useEffect(() => {
-    fetch(`${API_BASE_URL}/get_events.php`)
-      .then((response) => response.json())
-      .then((data) => {
-        if (Array.isArray(data)) {
-          setEvents(data.slice(0, 3));
-        } else {
-          console.error("La respuesta no es un arreglo de eventos válido");
-        }
-      })
-      .catch((error) => console.error("Error al obtener eventos:", error));
   }, []);
 
   // Obtener información del líder
@@ -215,139 +236,225 @@ function Home() {
 
 
       {/* Sección de Miembros (no el líder) */}
-      <section className="min-h-[600px] relative bg-dark text-white p-8">
-  <div className="content container mx-auto relative flex flex-col items-center gap-4">
-    {selectedMember ? (
-      <div className="grid grid-cols-2 items-center w-full max-w-5xl">
-        {/* Card de información */}
-        <div className="bg-blue-900 text-white rounded-l-lg shadow-lg p-6 h-[350px] w-full flex flex-col justify-between">
-          <div>
-            <h3 className="text-2xl font-bold mb-2">{selectedMember.tipo_miembro}</h3>
-            <h3 className="text-xl mb-2">{selectedMember.nombre_miembro}</h3>
-            <h2 className="text-base font-normal mb-4">{selectedMember.descripcion_miembro}</h2>
-          </div>
-          <div className="border-b-2 border-fuchsia-500 my-2"></div>
-          <div className="flex space-x-4">
-            <i
-              className="fab fa-facebook text-3xl text-blue-600 hover:scale-110 cursor-pointer transition-transform"
-              onClick={() => window.location.href = selectedMember.facebook_url}
-            ></i>
-            <i
-              className="fab fa-instagram text-3xl text-pink-500 hover:scale-110 cursor-pointer transition-transform"
-              onClick={() => window.location.href = selectedMember.instagram_url}
-            ></i>
+      <section className="min-h-[600px] relative bg-dark text-white p-8 mb-0">
+        <div className="content container mx-auto relative flex flex-col items-center gap-4">
+          {selectedMember ? (
+            <div className="grid grid-cols-2 items-center w-full max-w-5xl">
+              {/* Card de información */}
+              <div className="bg-blue-900 text-white rounded-l-lg shadow-lg p-6 h-[350px] w-full flex flex-col justify-between">
+                <div>
+                  <h3 className="text-2xl font-bold mb-2">{selectedMember.tipo_miembro}</h3>
+                  <h3 className="text-xl mb-2">{selectedMember.nombre_miembro}</h3>
+                  <h2 className="text-base font-normal mb-4">{selectedMember.descripcion_miembro}</h2>
+                </div>
+                <div className="border-b-2 border-fuchsia-500 my-2"></div>
+                <div className="flex space-x-4">
+                  <i
+                    className="fab fa-facebook text-3xl text-blue-600 hover:scale-110 cursor-pointer transition-transform"
+                    onClick={() => window.location.href = selectedMember.facebook_url}
+                  ></i>
+                  <i
+                    className="fab fa-instagram text-3xl text-pink-500 hover:scale-110 cursor-pointer transition-transform"
+                    onClick={() => window.location.href = selectedMember.instagram_url}
+                  ></i>
+                  <button
+                    className="bg-fuchsia-500 text-white px-4 py-2 rounded-lg text-base font-semibold hover:scale-110 transition-transform"
+                    onClick={() => window.location.href = selectedMember.ver_mas_url}
+                  >
+                    Ver más
+                  </button>
+                </div>
+              </div>
+
+              {/* Imagen */}
+              <div className="w-[350px] h-[350px] flex">
+                <img
+                  src={selectedMember.url_to_image_placeholder}
+                  alt={selectedMember.nombre_miembro}
+                  className="w-full h-full object-cover rounded-r-lg"
+                />
+              </div>
+            </div>
+          ) : (
+            <p className="text-center text-gray-400">Cargando miembro...</p>
+          )}
+
+          {/* Botones de navegación */}
+          <div className="flex gap-4 mt-4">
             <button
-              className="bg-fuchsia-500 text-white px-4 py-2 rounded-lg text-base font-semibold hover:scale-110 transition-transform"
-              onClick={() => window.location.href = selectedMember.ver_mas_url}
+              onClick={prevMember}
+              className="bg-blue-950 text-white w-12 h-12 flex items-center justify-center rounded-full hover:bg-fuchsia-800"
             >
-              Ver más
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-6 h-6"
+              >
+                <path d="M14 7l-5 5 5 5" />
+              </svg>
+            </button>
+            <button
+              onClick={nextMember}
+              className="bg-blue-950 text-white w-12 h-12 flex items-center justify-center rounded-full hover:bg-fuchsia-800"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-6 h-6"
+              >
+                <path d="M10 17l5-5-5-5" />
+              </svg>
             </button>
           </div>
         </div>
-
-        {/* Imagen */}
-        <div className="w-[350px] h-[350px] flex">
-          <img
-            src={selectedMember.url_to_image_placeholder}
-            alt={selectedMember.nombre_miembro}
-            className="w-full h-full object-cover rounded-r-lg"
-          />
-        </div>
-      </div>
-    ) : (
-      <p className="text-center text-gray-400">Cargando miembro...</p>
-    )}
-
-    {/* Botones de navegación */}
-    <div className="flex gap-4 mt-4">
-      <button
-        onClick={prevMember}
-        className="bg-blue-950 text-white w-12 h-12 flex items-center justify-center rounded-full hover:bg-fuchsia-800"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="w-6 h-6"
-        >
-          <path d="M14 7l-5 5 5 5" />
-        </svg>
-      </button>
-      <button
-        onClick={nextMember}
-        className="bg-blue-950 text-white w-12 h-12 flex items-center justify-center rounded-full hover:bg-fuchsia-800"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="w-6 h-6"
-        >
-          <path d="M10 17l5-5-5-5" />
-        </svg>
-      </button>
-    </div>
-  </div>
-</section>
-
-
+      </section>
 
       {/* Sección de Noticias */}
-      <section className="news-section">
-        <div className="content">
-          <p className="subtitle">NOTICIAS</p>
-          <h1>Últimas Actualizaciones y Noticias</h1>
-          <p>
-            Mantente al día con las últimas noticias y desarrollos de nuestra
-            campaña.
-          </p>
+      <section className="py-8 bg-gray-100 text-center mt-0">
+        <div className="content mb-6">
+          <h1 className="mt-4 text-4xl font-semibold">Noticias Relevantes</h1>
         </div>
-        <div className="news-items">
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 max-w-7xl mx-auto px-16">
           {news.length > 0 ? (
-            news.map((item, index) => (
-              <div className="news-item" key={index}>
-                <img src={item.urlImagen} alt={item.titulo} />
-                <h3>{item.titulo}</h3>
-                <p>{item.descripcion}</p>
+            news.slice(0, 4).map((item, index) => (
+              <div
+                className={`flex flex-col border rounded-lg overflow-hidden shadow-md bg-blue-900 ${index < 2 ? "col-span-1" : "col-span-1"}`}
+                key={index}
+              >
+                {/* Header con fecha */}
+                <div className="flex items-center bg-red-600 text-white px-4 py-2">
+                  <span className="mr-2">Fecha</span>
+                  <span className="font-medium">{item.fecha}</span>
+                </div>
+
+                {/* Imagen */}
+                <img
+                  className="w-full h-48 object-cover"
+                  src={item.urlImagen}
+                  alt={item.titulo}
+                />
+
+                {/* Cuerpo de la noticia */}
+                <div className="text-white p-4 flex-1 text-left">
+                  <h3 className="text-white font-semibold mb-2">{item.titulo_evento_noticia}</h3>
+                  <p className="text-white">{item.descrip_evento_noticia}</p>
+                </div>
+
+                {/* Botón Leer más */}
+                <button className="mt-6 mb-6 bg-blue-700 text-white py-2 px-4 w-max mx-auto rounded-md hover:bg-blue-800">
+                  Leer más
+                </button>
               </div>
             ))
           ) : (
-            <p>Cargando noticias...</p>
+            <p className="text-gray-500">Cargando noticias...</p>
           )}
         </div>
       </section>
 
-      {/* Sección de Propuestas */}
-      <section className="news-section">
-        <div className="content">
-          <p className="subtitle">PROPUETAS DESTACADAS</p>
-          <div className="news-items">
-            {proposals.length > 0 ? (
-              proposals.map((proposal, index) => (
-                <div className="news-item" key={index}>
-                  <h3>{proposal.titulo_propuesta}</h3>
-                  <p>{proposal.descripcion_propuesta}</p>
-                  <p>
-                    <strong>Alcance:</strong> {proposal.alcance_propuesta}
-                  </p>
+
+      {/* Sección de Eventos */}
+      <section className="py-8 bg-gray-100 text-center">
+        <div className="content mb-6">
+          <h1 className="mt-4 text-4xl font-semibold">Ultimos eventos</h1>
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 max-w-7xl mx-auto px-16">
+          {events.length > 0 ? (
+            events.slice(0, 4).map((item, index) => (
+              <div
+                className={`flex flex-col border rounded-lg overflow-hidden shadow-md bg-blue-900 ${index < 2 ? "col-span-1" : "col-span-1"}`}
+                key={index}
+              >
+                {/* Header con fecha */}
+                <div className="flex items-center bg-red-600 text-white px-4 py-2">
+                  <span className="mr-2">Fecha</span>
+                  <span className="font-medium">{item.fecha}</span>
                 </div>
-              ))
-            ) : (
-              <p>Cargando propuestas...</p>
-            )}
-          </div>
+
+                {/* Imagen */}
+                <img
+                  className="w-full h-48 object-cover"
+                  src={item.urlImagen}
+                  alt={item.titulo}
+                />
+
+                {/* Cuerpo del evento */}
+                <div className="text-white p-4 flex-1 text-left">
+                  <h3 className="text-white font-semibold mb-2">{item.titulo_evento_noticia}</h3>
+                  <p className="text-white">{item.descrip_evento_noticia}</p>
+                </div>
+
+                {/* Botón Leer más */}
+                <button className="mt-6 mb-6 bg-blue-700 text-white py-2 px-4 w-max mx-auto rounded-md hover:bg-blue-800">
+                  Leer más
+                </button>
+
+              </div>
+            ))
+          ) : (
+            <p className="text-gray-500">Cargando eventos...</p>
+          )}
+        </div>
+      </section>
+
+
+      {/* Sección de Propuestas */}
+      <section className="py-8 bg-gray-100 text-center">
+        <div className="content mb-6">
+          <h1 className="mt-4 text-4xl font-semibold">Propuestas Destacadas</h1>
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 max-w-7xl mx-auto px-16">
+          {proposals.length > 0 ? (
+            proposals.map((proposal, index) => (
+              <div
+                className={`flex flex-col border rounded-lg overflow-hidden shadow-md bg-blue-900 ${index < 2 ? "col-span-1" : "col-span-1"}`}
+                key={index}
+              >
+                {/* Header con información relevante */}
+                <div className="flex items-center bg-red-600 text-white px-4 py-2">
+                  <span className="mr-2">Alcance:</span>
+                  <span className="font-medium">{proposal.alcance_propuesta}</span>
+                </div>
+
+                {/* Imagen de la propuesta */}
+                <img
+                  className="w-full h-48 object-cover"
+                  src={proposal.img_url}  // Reemplaza con la URL de la imagen de la propuesta
+                  alt={proposal.titulo_propuesta}
+                />
+
+                {/* Cuerpo de la propuesta */}
+                <div className="text-white p-4 flex-1 text-left">
+                  <h3 className="text-white font-semibold mb-2">{proposal.titulo_propuesta}</h3>
+                  <p className="text-white">{proposal.descripcion_propuesta}</p>
+                </div>
+
+                {/* Botón Leer más */}
+                <button className="mt-6 mb-6 bg-blue-700 text-white py-2 px-4 w-max mx-auto rounded-md hover:bg-blue-800">
+                  Leer más
+                </button>
+              </div>
+            ))
+          ) : (
+            <p className="text-gray-500">Cargando propuestas...</p>
+          )}
         </div>
       </section>
 
