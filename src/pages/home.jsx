363d9448021b2570f45629a2a9a15e48b5588119
IsmaelSailema20 <ismaelsailema110@gmail.com>
2025-01-05 20:25:15 -0500

    cambios de puertos en la ruta de las api

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,7 +9,8 @@ function Home() {
   const [proposals, setProposals] = useState([]);
   const [sections, setSections] = useState([]); // Estado para almacenar las secciones
   const [informacion, setInformacion] = useState([]);
-  const API_BASE_URL = "http://localhost/ProyectoManejo/PaginaWebCandidata/models";
+  const API_BASE_URL =
+    "http://localhost:8081/ProyectoManejo/PaginaWebCandidata/models";
   const [selectedMember, setSelectedMember] = useState(members[0]);
   // Aquí debería estar la lista de miembros
 
@@ -28,14 +29,14 @@ function Home() {
 
   const nextMember = () => {
     const currentIndex = members.indexOf(selectedMember);
-    const nextIndex = (currentIndex + 1) % members.length;  // Si llega al final, vuelve al primero
+    const nextIndex = (currentIndex + 1) % members.length; // Si llega al final, vuelve al primero
     setSelectedMember(members[nextIndex]);
   };
 
   // Función para ir al miembro anterior
   const prevMember = () => {
     const currentIndex = members.indexOf(selectedMember);
-    const prevIndex = (currentIndex - 1 + members.length) % members.length;  // Si llega al primero, vuelve al último
+    const prevIndex = (currentIndex - 1 + members.length) % members.length; // Si llega al primero, vuelve al último
     setSelectedMember(members[prevIndex]);
   };
 
@@ -63,14 +64,14 @@ function Home() {
     fetch(`${API_BASE_URL}/get_news.php`)
       .then((response) => response.json())
       .then((data) => {
-        const updatedNews = data.map(item => {
+        const updatedNews = data.map((item) => {
           const words = item.descrip_evento_noticia.split(" ");
           const truncatedDescription = words.slice(0, 10).join(" "); // Cortar las primeras 10 palabras
           return {
             ...item,
             descrip_evento_noticia: truncatedDescription.endsWith("...")
               ? truncatedDescription // Si ya tiene puntos al final, no hacer nada
-              : `${truncatedDescription}...` // Si no, agregar exactamente tres puntos
+              : `${truncatedDescription}...`, // Si no, agregar exactamente tres puntos
           };
         });
         setNews(updatedNews); // Actualizar el estado con las noticias procesadas.
@@ -84,14 +85,14 @@ function Home() {
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        const updatedEvents = data.map(item => {
+        const updatedEvents = data.map((item) => {
           const words = item.descrip_evento_noticia.split(" ");
           const truncatedDescription = words.slice(0, 10).join(" "); // Cortar las primeras 10 palabras
           return {
             ...item,
             descrip_evento_noticia: truncatedDescription.endsWith("...")
               ? truncatedDescription // Si ya tiene puntos al final, no hacer nada
-              : `${truncatedDescription}...` // Si no, agregar exactamente tres puntos
+              : `${truncatedDescription}...`, // Si no, agregar exactamente tres puntos
           };
         });
         setEvents(updatedEvents); // Actualizar el estado con los eventos procesados.
@@ -138,7 +139,6 @@ function Home() {
       .catch((error) => console.error("Error al obtener propuestas:", error));
   }, []);
 
-
   return (
     <>
       <link
@@ -169,7 +169,7 @@ function Home() {
             top: 0,
             left: 0,
             zIndex: -1,
-            filter: "brightness(0.5) blur(2px)"
+            filter: "brightness(0.5) blur(2px)",
           }}
         ></div>
 
@@ -202,10 +202,17 @@ function Home() {
                     textAlign: "center",
                   }}
                 >
-                  {informacion['Nombre del partido']}
+                  {informacion["Nombre del partido"]}
                 </h3>
-                <h3 style={{ fontSize: "1.5rem", lineHeight: "1.5", marginTop: "10px", textAlign: "center" }}>
-                  {informacion['Slogan del partido']}
+                <h3
+                  style={{
+                    fontSize: "1.5rem",
+                    lineHeight: "1.5",
+                    marginTop: "10px",
+                    textAlign: "center",
+                  }}
+                >
+                  {informacion["Slogan del partido"]}
                 </h3>
               </div>
             )}
@@ -232,8 +239,14 @@ function Home() {
           <div style={{ flex: 1 }}>
             {leader && (
               <div style={{ marginTop: "50px", textAlign: "center" }}>
-
-                <h3 style={{ fontSize: "2.1rem", color: "fuchsia", lineHeight: "1.5", marginTop: "30px" }}>
+                <h3
+                  style={{
+                    fontSize: "2.1rem",
+                    color: "fuchsia",
+                    lineHeight: "1.5",
+                    marginTop: "30px",
+                  }}
+                >
                   {leader.descripcion_miembro}
                 </h3>
                 <h3 style={{ fontSize: "2rem", marginBottom: "30px" }}>
@@ -245,7 +258,6 @@ function Home() {
         </div>
       </section>
 
-
       {/* Sección de Miembros (no el líder) */}
       <section className="min-h-[600px] relative bg-dark text-white p-8 mb-0">
         <div className="content container mx-auto relative flex flex-col items-center gap-4">
@@ -254,23 +266,35 @@ function Home() {
               {/* Card de información */}
               <div className="bg-blue-900 text-white rounded-l-lg shadow-lg p-6 h-[350px] w-full flex flex-col justify-between">
                 <div>
-                  <h3 className="text-2xl font-bold mb-2">{selectedMember.tipo_miembro}</h3>
-                  <h3 className="text-xl mb-2">{selectedMember.nombre_miembro}</h3>
-                  <h2 className="text-base font-normal mb-4">{selectedMember.descripcion_miembro}</h2>
+                  <h3 className="text-2xl font-bold mb-2">
+                    {selectedMember.tipo_miembro}
+                  </h3>
+                  <h3 className="text-xl mb-2">
+                    {selectedMember.nombre_miembro}
+                  </h3>
+                  <h2 className="text-base font-normal mb-4">
+                    {selectedMember.descripcion_miembro}
+                  </h2>
                 </div>
                 <div className="border-b-2 border-fuchsia-500 my-2"></div>
                 <div className="flex space-x-4">
                   <i
                     className="fab fa-facebook text-3xl text-blue-600 hover:scale-110 cursor-pointer transition-transform"
-                    onClick={() => window.location.href = selectedMember.facebook_url}
+                    onClick={() =>
+                      (window.location.href = selectedMember.facebook_url)
+                    }
                   ></i>
                   <i
                     className="fab fa-instagram text-3xl text-pink-500 hover:scale-110 cursor-pointer transition-transform"
-                    onClick={() => window.location.href = selectedMember.instagram_url}
+                    onClick={() =>
+                      (window.location.href = selectedMember.instagram_url)
+                    }
                   ></i>
                   <button
                     className="bg-fuchsia-500 text-white px-4 py-2 rounded-lg text-base font-semibold hover:scale-110 transition-transform"
-                    onClick={() => window.location.href = selectedMember.ver_mas_url}
+                    onClick={() =>
+                      (window.location.href = selectedMember.ver_mas_url)
+                    }
                   >
                     Ver más
                   </button>
@@ -344,7 +368,9 @@ function Home() {
           {news.length > 0 ? (
             news.slice(0, 4).map((item, index) => (
               <div
-                className={`flex flex-col border rounded-lg overflow-hidden shadow-md bg-blue-900 ${index < 2 ? "col-span-1" : "col-span-1"}`}
+                className={`flex flex-col border rounded-lg overflow-hidden shadow-md bg-blue-900 ${
+                  index < 2 ? "col-span-1" : "col-span-1"
+                }`}
                 key={index}
               >
                 {/* Header con fecha */}
@@ -362,7 +388,9 @@ function Home() {
 
                 {/* Cuerpo de la noticia */}
                 <div className="text-white p-4 flex-1 text-left">
-                  <h3 className="text-white font-semibold mb-2">{item.titulo_evento_noticia}</h3>
+                  <h3 className="text-white font-semibold mb-2">
+                    {item.titulo_evento_noticia}
+                  </h3>
                   <p className="text-white">{item.descrip_evento_noticia}</p>
                 </div>
 
@@ -378,7 +406,6 @@ function Home() {
         </div>
       </section>
 
-
       {/* Sección de Eventos */}
       <section className="py-8 bg-gray-100 text-center">
         <div className="content mb-6">
@@ -389,7 +416,9 @@ function Home() {
           {events.length > 0 ? (
             events.slice(0, 4).map((item, index) => (
               <div
-                className={`flex flex-col border rounded-lg overflow-hidden shadow-md bg-blue-900 ${index < 2 ? "col-span-1" : "col-span-1"}`}
+                className={`flex flex-col border rounded-lg overflow-hidden shadow-md bg-blue-900 ${
+                  index < 2 ? "col-span-1" : "col-span-1"
+                }`}
                 key={index}
               >
                 {/* Header con fecha */}
@@ -407,7 +436,9 @@ function Home() {
 
                 {/* Cuerpo del evento */}
                 <div className="text-white p-4 flex-1 text-left">
-                  <h3 className="text-white font-semibold mb-2">{item.titulo_evento_noticia}</h3>
+                  <h3 className="text-white font-semibold mb-2">
+                    {item.titulo_evento_noticia}
+                  </h3>
                   <p className="text-white">{item.descrip_evento_noticia}</p>
                 </div>
 
@@ -415,7 +446,6 @@ function Home() {
                 <button className="mt-6 mb-6 bg-blue-700 text-white py-2 px-4 w-max mx-auto rounded-md hover:bg-blue-800">
                   Leer más
                 </button>
-
               </div>
             ))
           ) : (
@@ -423,7 +453,6 @@ function Home() {
           )}
         </div>
       </section>
-
 
       {/* Sección de Propuestas */}
       <section className="py-8 bg-gray-100 text-center">
@@ -435,27 +464,37 @@ function Home() {
           {proposals.length > 0 ? (
             proposals.map((proposal, index) => (
               <div
-                className={`flex flex-col border rounded-lg overflow-hidden shadow-md ${index < 2 ? "col-span-1" : "col-span-1"}`}
+                className={`flex flex-col border rounded-lg overflow-hidden shadow-md ${
+                  index < 2 ? "col-span-1" : "col-span-1"
+                }`}
                 key={index}
               >
                 {/* Header con información relevante */}
                 <div className="flex items-center bg-green-600 text-white px-4 py-2">
                   <span className="mr-2">Alcance:</span>
-                  <span className="font-medium">{proposal.alcance_propuesta.toUpperCase()}</span>
+                  <span className="font-medium">
+                    {proposal.alcance_propuesta.toUpperCase()}
+                  </span>
                 </div>
 
                 {/* Imagen de la propuesta */}
                 <img
                   className="w-full h-48 object-cover"
-                  src={proposal.img_url}  // Reemplaza con la URL de la imagen de la propuesta
+                  src={proposal.img_url} // Reemplaza con la URL de la imagen de la propuesta
                   alt={proposal.titulo_propuesta}
                 />
 
                 {/* Cuerpo de la propuesta */}
                 <div className="text-black p-4 flex-1 text-left">
-                  <h3 className="text-black font-semibold mb-2">{proposal.titulo_propuesta}</h3>
+                  <h3 className="text-black font-semibold mb-2">
+                    {proposal.titulo_propuesta}
+                  </h3>
                   <p className="text-black ">
-                    {proposal.descripcion_propuesta.split(' ').slice(0, 5).join(' ')}...
+                    {proposal.descripcion_propuesta
+                      .split(" ")
+                      .slice(0, 5)
+                      .join(" ")}
+                    ...
                   </p>
                 </div>
 
@@ -477,13 +516,17 @@ function Home() {
         {isModalOpen && selectedProposal && (
           <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-50">
             <div className="bg-white p-8 rounded-lg max-w-3xl w-full">
-              <h3 className="text-xl font-semibold mb-4 justify-start">{selectedProposal.titulo_propuesta}</h3>
+              <h3 className="text-xl font-semibold mb-4 justify-start">
+                {selectedProposal.titulo_propuesta}
+              </h3>
               <img
                 className="w-full h-48 object-cover mb-4"
                 src={selectedProposal.img_url}
                 alt={selectedProposal.titulo_propuesta}
               />
-              <p className="mb-4 justify-start">{selectedProposal.descripcion_propuesta}</p>
+              <p className="mb-4 justify-start">
+                {selectedProposal.descripcion_propuesta}
+              </p>
               <div className="flex justify-end">
                 <button
                   onClick={closeModal}
@@ -503,11 +546,19 @@ function Home() {
           <div className="space-y-6 text-white">
             {sections.length > 0 ? (
               sections.map((section, index) => (
-                <div className="flex items-center p-4 text-white border border-gray-300 rounded-lg shadow-sm space-x-6" key={index}>
+                <div
+                  className="flex items-center p-4 text-white border border-gray-300 rounded-lg shadow-sm space-x-6"
+                  key={index}
+                >
                   <div className="flex-1 bg-blue-900 text-white p-4 rounded-lg">
-                    <p className="text-center text-white font-semibold text-lg">{section.nombre}</p>
-                    <div className="mt-2 mb-4 border-b-2 border-fuchsia-500 w-full mx-auto"></div> {/* Cambié w-12 por w-full */}
-                    <p className="mt-2 mb-4 text-white">{section.descripcion}</p>
+                    <p className="text-center text-white font-semibold text-lg">
+                      {section.nombre}
+                    </p>
+                    <div className="mt-2 mb-4 border-b-2 border-fuchsia-500 w-full mx-auto"></div>{" "}
+                    {/* Cambié w-12 por w-full */}
+                    <p className="mt-2 mb-4 text-white">
+                      {section.descripcion}
+                    </p>
                   </div>
                   {section.url_de_la_imagen && (
                     <div className="flex-shrink-0">
@@ -526,10 +577,6 @@ function Home() {
           </div>
         </div>
       </section>
-
-
-
-
     </>
   );
 }
